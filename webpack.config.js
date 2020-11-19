5c08ad01d0871054ec86c8a1e2a4a9e8f0c36da3
Janko <mailodjanka@gmail.com>
2020-11-19 11:24:00 +0100

    Adds mode and devtool keys to webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: ""
   },
+  devtool: "cheap-module-eval-source-map"
 };
